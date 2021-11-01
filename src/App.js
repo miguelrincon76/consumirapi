11508b710b7a29b2db82edb4796d4079bb2c0dda
miguelrincon76 <miangelrincon@hotmail.com>
2021-11-01 17:29:08 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./assetss/css/App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return <div className="App">Hola mundo</div>;
+  return (
+    <div className="App">
+      <br />
+      <button className="btn btn-primary">Hola mundo</button>
+    </div>
+  );
 }
 
 export default App;
